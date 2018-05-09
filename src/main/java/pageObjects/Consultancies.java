f3db3d9bce64584c60e6d336973b5cca114a2df4
pirog <pirog@NEW-PC>
2018-05-09 23:21:44 +0300

    Not Working login page

--- src/main/java/pageObjects/Consultancies.java
+++ src/main/java/pageObjects/Consultancies.java
@@ -4,11 +4,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Consultancies {
-	WebDriver driver;
+	
+	private  WebDriver driver;
 	
 	By NewConsultancy=By.xpath(".//a[@href='/admin/consultancies/new']");
 	By Find=By.xpath(".//input[@placeholder='Поиск']");
-	By RefreshBtn=By.xpath(".//i[@class='glyphicon glyphicon-refresh icon-refresh']");
+	By PagenatorInfo=By.xpath("//span[@class='pagination-info']");
+	By ConsultancyName=By.xpath("//th[@data-field='name']");
+	By ConsultancyWorkload=By.xpath("//th[@data-field='workload']");
+	
+	
+	public Consultancies(WebDriver driver) {
+		this.driver = driver;
+		
+	}
+	
+	public String GetConsultancyNameValue(int rowNumber) {
+		return this.driver.findElement(ConsultancyName).getText();
+	}
+	
+	public String GetConsultancyWorkloadValue(int rowNumber) {
+		return this.driver.findElement(ConsultancyWorkload).getText();
+	}
+	
+	public String GetPagenatorInfoValue(int rowNumber) {
+		return this.driver.findElement(PagenatorInfo).getText();
+	}
+	public Consultancies GetToPage(String url) {
+		this.driver.get(url);
+		return this;
+	}
+	
+	
 
 
 }
